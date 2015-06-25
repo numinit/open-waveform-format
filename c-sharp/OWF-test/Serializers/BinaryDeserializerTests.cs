b2b176a4255a648796a3d99af292362c8d43b691
Morgan Jones <morgan@medicalinformaticscorp.com>
2015-06-25 17:34:25 -0500

    Write more tests; port serializer tests to use prebaked examples

--- c-sharp/OWF-test/Serializers/BinaryDeserializerTests.cs
+++ c-sharp/OWF-test/Serializers/BinaryDeserializerTests.cs
@@ -11,51 +11,64 @@ namespace OWF_test.Serializers
     [TestClass]
     public class BinaryDeserializerTests
     {
-        public byte[] ReadOWF(string filename)
+        private byte[] ReadOWF(string filename)
         {
             return System.IO.File.ReadAllBytes(String.Join("/", "..", "..", "..", "..", "example", "owf1_" + filename + ".owf"));
         }
+
         [TestMethod]
         public void DeserializesValidEmptyPacket()
         {
             byte[] buf = ReadOWF("binary_valid_empty");
-            try
-            {
-                Package p = BinaryDeserializer.convert(buf);
-                Assert.Equals(p.Channels.Count, 0);
-            }
-            catch (Exception e)
-            {
-                Assert.Fail(e.ToString());
-            }
+            Package p = BinaryDeserializer.convert(buf);
+            Assert.AreEqual(p.Channels.Count, 0);
+        }
+
+        [TestMethod]
+        public void DeserializesValidEmptyChannel()
+        {
+            byte[] buf = ReadOWF("binary_valid_empty_channel");
+            Package p = BinaryDeserializer.convert(buf);
+            Assert.AreEqual(p.Channels.Count, 1);
+            Assert.AreEqual(p.Channels[0].Id, "BED_42");
+            Assert.AreEqual(p.Channels[0].Namespaces.Count, 0);
+        }
+
+        [TestMethod]
+        public void DeserializesValidEmptyNamespace()
+        {
+            byte[] buf = ReadOWF("binary_valid_empty_namespace");
+            Package p = BinaryDeserializer.convert(buf);
+            Assert.AreEqual(p.Channels.Count, 1);
+            Assert.AreEqual(p.Channels[0].Id, "BED_42");
+            Assert.AreEqual(p.Channels[0].Namespaces.Count, 1);
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Id, "GEWAVE");
+            Assert.AreEqual(p.Channels[0].Namespaces[0].dt, new TimeSpan(0, 0, 3));
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Signals.Count, 0);
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Events.Count, 0);
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Alarms.Count, 0);
         }
 
         [TestMethod]
         public void DeserializesValidPacket1()
         {
             byte[] buf = ReadOWF("binary_valid_1");
-            try
-            {
-                Package p = BinaryDeserializer.convert(buf);
-                Assert.Equals(p.Channels.Count, 1);
-                Assert.Equals(p.Channels[0].Id, "BED_42");
-                Assert.Equals(p.Channels[0].Namespaces.Count, 1);
-                Assert.Equals(p.Channels[0].Namespaces[0].Id, "GEWAVE");
-                Assert.Equals(p.Channels[0].Namespaces[0].Signals.Count, 1);
-                Assert.Equals(p.Channels[0].Namespaces[0].Signals[0].Id, "ECG_LEAD_2");
-                Assert.Equals(p.Channels[0].Namespaces[0].Signals[0].Unit, "mV");
-                Assert.Equals(p.Channels[0].Namespaces[0].Signals[0].Samples.Length, 11);
-                Assert.Equals(p.Channels[0].Namespaces[0].Events[0].Data, "POST OK");
-                Assert.Equals(p.Channels[0].Namespaces[0].Alarms.Count, 1);
-                Assert.Equals(p.Channels[0].Namespaces[0].Alarms[0].Level, 0);
-                Assert.Equals(p.Channels[0].Namespaces[0].Alarms[0].Volume, 255);
-                Assert.Equals(p.Channels[0].Namespaces[0].Alarms[0].Type, "SPO2 LO");
-                Assert.Equals(p.Channels[0].Namespaces[0].Alarms[0].Message, "43");
-            }
-            catch (Exception e)
-            {
-                Assert.Fail(e.ToString());
-            }
+            Package p = BinaryDeserializer.convert(buf);
+            Assert.AreEqual(p.Channels.Count, 1);
+            Assert.AreEqual(p.Channels[0].Id, "BED_42");
+            Assert.AreEqual(p.Channels[0].Namespaces.Count, 1);
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Id, "GEWAVE");
+            Assert.AreEqual(p.Channels[0].Namespaces[0].dt, new TimeSpan(0, 0, 3));
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Signals.Count, 1);
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Signals[0].Id, "ECG_LEAD_2");
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Signals[0].Unit, "mV");
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Signals[0].Samples.Length, 11);
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Events[0].Data, "POST OK");
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Alarms.Count, 1);
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Alarms[0].Level, 0);
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Alarms[0].Volume, 255);
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Alarms[0].Type, "SPO2 LO");
+            Assert.AreEqual(p.Channels[0].Namespaces[0].Alarms[0].Message, "43");
         }
     }
 }
--- c-sharp/OWF-test/Serializers/BinarySerializerTests.cs
+++ c-sharp/OWF-test/Serializers/BinarySerializerTests.cs
@@ -11,48 +11,42 @@ namespace OWF_test.Serializers
     [TestClass]
     public class BinarySerializerTests
     {
+        private byte[] ReadOWF(string filename)
+        {
+            return System.IO.File.ReadAllBytes(String.Join("/", "..", "..", "..", "..", "example", "owf1_" + filename + ".owf"));
+        }
+
         [TestMethod]
         public void GeneratesCorrectEmptyObject()     
         {
             var p = new Package(new List<Channel>());
             byte[] buffer = BinarySerializer.convert(p);
-            byte[] expected = new byte[] {0x4f, 0x57, 0x46, 0x31, /**/ 0, 0, 0, 0};
+            byte[] expected = ReadOWF("binary_valid_empty");
             CollectionAssert.AreEqual(buffer, expected, "Incorrect empty object");
         }
 
         [TestMethod]
         public void GeneratesCorrectEmptyChannelObject()
         {
-            var c = new Channel("TEST_CHANNEL", new List<Namespace>());
+            var c = new Channel("BED_42", new List<Namespace>());
             var p = new Package(new List<Channel>(new Channel[] { c }));
             byte[] buffer = BinarySerializer.convert(p);
-            byte[] expected = {
-                0x4f, 0x57, 0x46, 0x31, 0x00, 0x00, 0x00, 0x18, 0x00, 0x00, 0x00, 0x14,
-                0x00, 0x00, 0x00, 0x10, 0x54, 0x45, 0x53, 0x54, 0x5f, 0x43, 0x48, 0x41,
-                0x4e, 0x4e, 0x45, 0x4c, 0x00, 0x00, 0x00, 0x00
-            };
+            byte[] expected = ReadOWF("binary_valid_empty_channel");
             CollectionAssert.AreEqual(buffer, expected, "Incorrect empty channel");
         }
 
         [TestMethod]
         public void GeneratesCorrectEmptyNamespaceObject()
         {
-            var t0 = new DateTime(2015, 6, 8, 5, 30, 22);
-            var dt = new TimeSpan(1,2,3);
+            // TODO: This may be incorrect.
+            var t0 = new DateTime(2015, 6, 4, 16, 59, 4, 3018);
+            var dt = new TimeSpan(0, 0, 3);
 
-            var n = new Namespace("TEST_NAMESPACE", t0, dt, new List<Signal>(), new List<Event>(), new List<Alarm>());
-            var c = new Channel("TEST_CHANNEL", new List<Namespace>(new Namespace[] { n }));
+            var n = new Namespace("GEWAVE", t0, dt, new List<Signal>(), new List<Event>(), new List<Alarm>());
+            var c = new Channel("BED_42", new List<Namespace>(new Namespace[] { n }));
             var p = new Package(new List<Channel>(new Channel[] { c }));
             byte[] buffer = BinarySerializer.convert(p);
-            byte[] expected = {
-                0x4f, 0x57, 0x46, 0x31, 0x00, 0x00, 0x00, 0x4c, 0x00, 0x00, 0x00, 0x48,
-                0x00, 0x00, 0x00, 0x10, 0x54, 0x45, 0x53, 0x54, 0x5f, 0x43, 0x48, 0x41,
-                0x4e, 0x4e, 0x45, 0x4c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x30,
-                0x00, 0x32, 0xef, 0xcd, 0x61, 0x94, 0xab, 0x00, 0x00, 0x00, 0x00, 0x00,
-                0x00, 0x38, 0xce, 0xf8, 0x00, 0x00, 0x00, 0x10, 0x54, 0x45, 0x53, 0x54,
-                0x5f, 0x4e, 0x41, 0x4d, 0x45, 0x53, 0x50, 0x41, 0x43, 0x45, 0x00, 0x00,
-                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-            };
+            byte[] expected = ReadOWF("binary_valid_empty_namespace");
             CollectionAssert.AreEqual(buffer, expected, "Incorrect empty namespace");
         }
     }
